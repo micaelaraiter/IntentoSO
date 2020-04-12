611b85d51fff2cd7cc19d23e96fdd88ffcff9439
micaelaraiter <micaelaraiter@gmail.com>
2020-04-11 21:06:31 -0300

    intento de unir, los dos proyectos

--- Team/utils.c
+++ Team/utils.c
@@ -74,22 +74,111 @@ void enviar_mensaje(char* mensaje, int socket_cliente)
 	free(paquete);
 	free(stream);
 }
+
 //TODO
-char* recibir_mensaje(int socket_cliente)
+void* recibir_mensaje(int socket_cliente, int* size)
 {
-	int bytes_recibidos;
-	char* buffer = malloc(10);
-
-	bytes_recibidos = recv(socket_cliente, buffer, 9 , 0);
-
-	buffer[bytes_recibidos] = '\0';
-
-	free(buffer);
-
+	void * buffer;
+	log_info(logger,"Recibiendo mensaje.");
+	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
+	log_info(logger,"Tamaño de paquete recibido: %d",*size);
+	buffer = malloc(*size);
+	recv(socket_cliente, buffer, *size, MSG_WAITALL);
+	log_info(logger,"Mensaje recibido.");
 	return buffer;
 }
 
 void liberar_conexion(int socket_cliente)
 {
 	close(socket_cliente);
+}
+
+void iniciar_servidor(char *IP, char *PUERTO)
+{
+	int socket_servidor;
+
+    struct addrinfo hints, *servinfo, *p;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo(IP, PUERTO, &hints, &servinfo);
+
+    for (p=servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((socket_servidor = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+            continue;
+
+        if (bind(socket_servidor, p->ai_addr, p->ai_addrlen) == -1) {
+            close(socket_servidor);
+            continue;
+        }
+        break;
+    }
+
+	listen(socket_servidor, SOMAXCONN);
+
+    freeaddrinfo(servinfo);
+    log_info(logger,"Servidor levantdo.");
+    while(1)
+    	esperar_cliente(socket_servidor);
+}
+
+void esperar_cliente(int socket_servidor)
+{
+	struct sockaddr_in dir_cliente;
+
+	int tam_direccion = sizeof(struct sockaddr_in);
+
+	int socket_cliente = accept(socket_servidor, (void*) &dir_cliente, &tam_direccion);
+	pthread_create(&thread,NULL,(void*)serve_client,&socket_cliente);
+	pthread_detach(thread);
+
+}
+
+void serve_client(int* socket)
+{
+	int cod_op;
+	if(recv(*socket, &cod_op, sizeof(int), MSG_WAITALL) == -1)
+		cod_op = -1;
+	log_info(logger,"Se conecto un cliente con socket: %d",*socket);
+	process_request(cod_op, *socket);
+}
+
+void process_request(int cod_op, int cliente_fd) {
+	int size;
+	void* msg;
+	log_info(logger,"Codigo de operacion %d",cod_op);
+	switch (cod_op) {
+		case MENSAJE:
+			msg = recibir_mensaje(cliente_fd, &size);
+			devolver_mensaje(msg, size, cliente_fd);
+			free(msg);
+			break;
+		case 0:
+			pthread_exit(NULL);
+		case -1:
+			pthread_exit(NULL);
+	}
+}
+
+void devolver_mensaje(void* payload, int size, int socket_cliente)
+{
+	log_info(logger,"Devolviendo mensaje");
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->codigo_operacion = MENSAJE;
+	paquete->buffer = malloc(sizeof(t_buffer));
+	paquete->buffer->size = size;
+	paquete->buffer->stream = malloc(paquete->buffer->size);
+	memcpy(paquete->buffer->stream, payload, paquete->buffer->size);
+	int bytes = paquete->buffer->size + 2*sizeof(int);
+	void* a_enviar = serializar_paquete(paquete, bytes);
+	send(socket_cliente, a_enviar, bytes, 0);
+	free(a_enviar);
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+	log_info(logger,"Mensaje devuelto");
 }
--- Team/utils.h
+++ Team/utils.h
@@ -10,12 +10,15 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<signal.h>
-#include<unistd.h>
 #include<sys/socket.h>
+#include<unistd.h>
 #include<netdb.h>
-#include<string.h>
+#include<signal.h>
 #include<commons/log.h>
+#include<commons/collections/list.h>
+#include<string.h>
+#include<pthread.h>
+
 typedef enum
 {
 	MENSAJE = 1,
@@ -33,12 +36,24 @@ typedef struct
 	t_buffer* buffer;
 } t_paquete;
 
+pthread_t thread;
 t_log* logger;
 
+void* serializar_paquete(t_paquete* paquete, int bytes);
 int crear_conexion(char* ip, char* puerto);
 void enviar_mensaje(char* mensaje, int socket_cliente);
-char* recibir_mensaje(int socket_cliente);
+void* recibir_mensaje(int socket_cliente, int* size)
 void eliminar_paquete(t_paquete* paquete);
 void liberar_conexion(int socket_cliente);
+
+void* recibir_buffer(int*, int); /// este no esta definido en utils.c
+
+void iniciar_servidor(char *IP, char *PUERTO);
+void esperar_cliente(int);
+void serve_client(int *socket);
+int recibir_operacion(int);
+void process_request(int cod_op, int cliente_fd);
+
+void devolver_mensaje(void* payload, int size, int socket_cliente);
 
 #endif /* UTILS_H_ */
