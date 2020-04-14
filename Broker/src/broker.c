5ca4d626795ee6efbffe0cbe9543a0a0c672885a
thomykut <thomy1999@gmail.com>
2020-04-13 23:25:01 -0300

    cast config_get_string_value() as strdup()

--- Broker/src/broker.c
+++ Broker/src/broker.c
@@ -33,19 +33,19 @@ t_config_broker* leer_config(t_log* logger) {
 
 	log_info(logger, "peso malloc config %d", sizeof(t_config_broker));
 
-	t_config_broker* config_broker = malloc(sizeof(t_config_broker)+13);
+	t_config_broker* config_broker = malloc(sizeof(t_config_broker));
 
 	 config = config_create("Debug/broker.config");
 
 	 config_broker -> sizeMemoria = config_get_int_value(config, "TAMANO_MEMORIA");
 	 config_broker -> sizeMinMemoria = config_get_int_value(config, "TAMANO_MEMORIA");
-	 config_broker -> algoritmoMemoria = config_get_string_value(config, "ALGORITMO_MEMORIA");
-	 config_broker -> algoritmoReemplazo = config_get_string_value(config, "ALGORITMO_REEMPLAZO");
-	 config_broker -> algoritmoParticionLibre = config_get_string_value(config, "ALGORITMO_PARTICION_LIBRE");
-	 config_broker -> ip_team = config_get_string_value(config, "IP_BROKER_TEAM");
-	 config_broker -> puerto_team = config_get_string_value(config, "PUERTO_BROKER_TEAM");
-	 config_broker -> ip_gamecard = config_get_string_value(config, "IP_BROKER_GAMECARD");
-	 config_broker -> puerto_gamecard = config_get_string_value(config, "PUERTO_BROKER_GAMECARD");
+	 config_broker -> algoritmoMemoria = strdup(config_get_string_value(config, "ALGORITMO_MEMORIA"));
+	 config_broker -> algoritmoReemplazo = strdup(config_get_string_value(config, "ALGORITMO_REEMPLAZO"));
+	 config_broker -> algoritmoParticionLibre = strdup(config_get_string_value(config, "ALGORITMO_PARTICION_LIBRE"));
+	 config_broker -> ip_team = strdup(config_get_string_value(config, "IP_BROKER_TEAM"));
+	 config_broker -> puerto_team = strdup(config_get_string_value(config, "PUERTO_BROKER_TEAM"));
+	 config_broker -> ip_gamecard = strdup(config_get_string_value(config, "IP_BROKER_GAMECARD"));
+	 config_broker -> puerto_gamecard = strdup(config_get_string_value(config, "PUERTO_BROKER_GAMECARD"));
 	 config_broker -> frecuenciaCompactacion = config_get_int_value(config, "FRECUENCIA_COMPACTACION");
 
 	 config_destroy(config);
